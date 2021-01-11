4cc4ae21f931b68266d0257354c554ded54d554e
Victor Garcia <v.garcia.mestre@gmail.com>
2021-01-11 12:08:29 +0100

    user saving in database created

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,43 @@ const bcrypt = require("bcrypt-nodejs");
 const User= require("../models/user");
 
 function signUp(req, res){
-    console.log('logeado');
+    const user= new User();
+
+    const {email, password, repeatPassword} = req.body;
+    user.email = email;
+    user.role= "admin";
+    user.active=false;
+
+    if(!password || !repeatPassword){
+        res.status(404).send({message:"Las contraseñas son obligatorias"})
+    }else{
+        if(password !== repeatPassword){
+            res.status(404).send({message:"La contraseña debe coincidir"})
+        }else{
+            bcrypt.hash(password, null, null, function(err, hash){
+                if (err){
+                    res.status(500).send({message:"error al encriptar la contraseña"})
+                } else{
+                    user.password= hash;
+                    user.save((err, userStored)=>{
+                        if(err){
+                            res.status(500).send({message:"Error del Servidor"})
+                        }else{
+                            if(!userStored){
+                                res.status(404).send({message:"Error al crear el usuario"})
+                            }else{
+                                res.status(200).send({user:userStored})
+                            }
+                        }
+
+                    })
+                }
+
+            })
+            
+        }
+    }
+ 
 }
 
 module.exports={
