a237cce90ff17e6414715ce9c6d464156b4b2561
Victor Garcia <v.garcia.mestre@gmail.com>
2021-01-31 20:37:55 +0100

    adding header configuration to server

--- server/app.js
+++ server/app.js
@@ -15,6 +15,16 @@ app.use(bodyParser.json())
 
 
 // configure head HTTP
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
 
 //BASIC ROUTES
 app.use(`/api/${API_VERSION}`,authRoutes)
