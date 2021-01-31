8f6f03eaba524b8b9998418159c6b1178b1e94ee
Victor Garcia <v.garcia.mestre@gmail.com>
2021-01-31 17:00:55 +0100

    endpoint de los tokens

--- server/app.js
+++ server/app.js
@@ -5,8 +5,9 @@ const app = express();
 const {API_VERSION} = require('./config');
 
 //...load routing//
-
+const authRoutes = require("./routers/auth")
 const userRoutes = require("./routers/user")
+
 
 
 app.use(bodyParser.urlencoded({extended: false}))
@@ -16,8 +17,9 @@ app.use(bodyParser.json())
 // configure head HTTP
 
 //BASIC ROUTES
-
+app.use(`/api/${API_VERSION}`,authRoutes)
 app.use(`/api/${API_VERSION}`, userRoutes)
+
 
 
 module.exports=app;
