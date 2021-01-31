0350d8e8aa1e882770f79b3c8e6fb848cdbcbc2d
Victor Garcia <v.garcia.mestre@gmail.com>
2021-01-31 20:24:03 +0100

    middleware to filter for only logged users

---
+++ server/middlewares/authenticated.js
@@ -0,0 +1,24 @@
+const jwt = require ('jwt-simple');
+const moment= require ('moment');
+
+const SECRET_KEY="123456";
+
+exports.ensureAuth = (req, res, next) =>{
+    if (!req.headers.authorization){
+        return res.status(403).send({message:"La peticion no tiene cabecera de autentificacion"})
+    }
+
+    const token = req.headers.authorization.replace(/['"]+/g, "");
+
+    try{
+        var payload = jwt.decode(token, SECRET_KEY);
+        if (payload.exp <= moment.unix() ){
+            return res.status(404).send({message:"El Token ha expirado"});
+        }
+    } catch(ex){
+       return res.status(404).send({message:"Token invalido"});
+    }
+
+    req.user= payload;
+    next();
+}
