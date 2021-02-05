54165fa0f3567073aee66a36bbabdd633da8bf4c
Victor Garcia <v.garcia.mestre@gmail.com>
2021-02-05 20:08:19 +0100

    Endpoint for menus created

--- server/controllers/menu.js
+++ server/controllers/menu.js
@@ -1,8 +1,26 @@
 const Menu = require('../models/menu');
 
 function addMenu(req,res){
-    console.log("add menu")
-}
+    const {title, url, order, active} = req.body;
+    const menu= new Menu();
+    menu.title= title;
+    menu.url=url;
+    menu.order=order;
+    menu.active=active;
+
+    menu.save((err, createdMenu)=>{
+        if(err){
+            res.status(500).send({message:"Error del servidor"})
+        } else{
+            if(!createdMenu){
+                res.status(404).send({message:"Error al crear el menu"})
+            } else{
+                res.status(200).send({message:"Menu creado correctamente"})
+            };
+        };
+    });
+
+};
 
 module.exports={
     addMenu
